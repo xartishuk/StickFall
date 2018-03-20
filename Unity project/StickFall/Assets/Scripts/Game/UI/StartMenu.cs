db6c133f13d1f1df91967054f90015c9368cd934
AntonMelekhavets <antohage@gmail.com>
2018-03-21 02:15:34 +0300

    Added main menu UI

---
+++ Unity project/StickFall/Assets/Scripts/Game/UI/StartMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour {
+	public void PlayPressed()
+	{
+		SceneManager.LoadScene("Main");
+	}
+
+	public void ExitPressed()
+	{
+		Application.Quit();
+	}
+}
